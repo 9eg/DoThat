c72fa5f01d6e728f944a8fae611f275a10f21b8a
Noah <101020515+9eg@users.noreply.github.com>
2022-03-10 22:47:35 -0600

    Create 'AssemblyInfo.cs' File
    Created the file that holds all the assembly information.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("DoThat")]
+[assembly: AssemblyDescription ("An automation tool for Windows. Write your own script to begin!")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Noah Bartley")]
+[assembly: AssemblyProduct ("DoThat")]
+[assembly: AssemblyCopyright ("Copyright (C) Noah Bartley / 9eg")]
+[assembly: AssemblyTrademark ("https://github.com/9eg/DoThat")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: AssemblyVersion ("0.0.0.1")]
+[assembly: AssemblyFileVersion ("0.0.0.1")]
+[assembly: Guid ("54145c77-f07d-43aa-9a73-ae63c7a2d017")]
+[assembly: NeutralResourcesLanguage ("en-US")]
